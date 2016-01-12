3e812f6a45a772e5ce105d59829d2885cd58abe6
yam-note <yam@yamnote>
2016-01-12 20:31:49 +0900

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name e2eIntroApp
+ * @description
+ * # e2eIntroApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('e2eIntroApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl as vm',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name e2eIntroApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the e2eIntroApp
+ */
+angular.module('e2eIntroApp').controller('MainCtrl', function () {
+  this.addTodo = function() {
+    this.todos.push(this.todo);
+  };
+
+
+  this.todos = ['item1', 'item2', 'item3'];
+});
