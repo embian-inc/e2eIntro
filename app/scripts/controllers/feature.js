d5228a247fb672406e7f61cc99e1a8a8d6210447
soeun <mojosoeun@gmail.com>
2016-01-14 16:07:12 +0900

    feature add

---
+++ app/scripts/controllers/feature.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name e2eIntroApp.controller: FeatureCtrl
+ * @description
+ * # FeatureCtrl
+ * Controller of the e2eIntroApp
+ */
+angular.module('e2eIntroApp')
+  .controller('FeatureCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
